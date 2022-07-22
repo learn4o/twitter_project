a826ed9df2dd6b306f79d9536f6644a48118336d
praveen kumar <prAveenkumar100700@gmail.com>
2022-07-22 17:01:46 +0530

    3 home icone

--- pages/sample.js
+++ pages/sample.js
@@ -1,12 +1,14 @@
-import Head from "next/head";
-import Image from "next/image";
-import HomeIcon from "../images/HomeIcon";
-import styles from "../styles/Sample.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import HomeIcon from '../images/HomeIcon';
+import styles from '../styles/Sample.module.css';
 
 export default function Sample() {
   return (
-    <div style={{ backgroundColor: "grey" }}>
+    <div style={{ backgroundColor: 'grey' }}>
       <div>Something</div>
+      <HomeIcon />
+      <HomeIcon />
       <HomeIcon />
     </div>
   );
