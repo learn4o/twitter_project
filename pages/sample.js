8817097f561607d4ae8542697289d7809fe969cc
praveen kumar <prAveenkumar100700@gmail.com>
2022-07-22 18:02:12 +0530

    myfirst

--- pages/sample.js
+++ pages/sample.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import HomeIcon from '../images/HomeIcon';
 import styles from '../styles/Sample.module.css';
+import First from './api/First';
 
 export default function Sample() {
   return (
@@ -9,8 +10,7 @@ export default function Sample() {
       <div>Something</div>
       <HomeIcon />
       <HomeIcon />
-      <HomeIcon /> <HomeIcon />
-      <HomeIcon />
+      <First />
     </div>
   );
 }
