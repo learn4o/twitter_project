075d56b09a595c5ae477afb42d2b092476093c3f
karthikkbdl4o <109502155+karthikkbdl4o@users.noreply.github.com>
2022-07-25 11:16:00 +0530

    Testing Heroku

--- pages/sample.js
+++ pages/sample.js
@@ -3,11 +3,7 @@ import HomeIcon from "../images/HomeIcon";
 export default function Sample() {
   return (
     <div style={{ backgroundColor: "grey" }}>
-      <div>Something</div>
-      <HomeIcon />
-      <HomeIcon /> <HomeIcon />
-      <HomeIcon /> <HomeIcon />
-      <HomeIcon />
+      <div>Changes</div>
     </div>
   );
 }
