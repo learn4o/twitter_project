52b758ef85c33a27f49c74d6ad79d51a148f56a0
TarunMandani <tarunmandanixig@gmail.com>
2022-07-28 10:24:51 +0530

    demoFile

---
+++ pages/demo.js
@@ -0,0 +1,7 @@
+import Media from "../components/Media";
+
+export default function Home() {
+    return (
+        Media({url:"https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8bGVuc3xlbnwwfHwwfHw%3D&w=1000&q=80", type:"image"})
+    )
+}
