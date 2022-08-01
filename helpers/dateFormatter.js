7664d7687b2c0819c963f32e84f46db634c79471
praveen kumar <prAveenkumar100700@gmail.com>
2022-08-01 14:17:37 +0530

    date add

---
+++ helpers/dateFormatter.js
@@ -0,0 +1,58 @@
+
+
+const dateFormatter = (date) => {
+      
+/////curent time
+
+
+var monthName =['','Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+
+
+
+//post date //////
+var postHour=03,postmint=04,postSec=33
+var postday=03,postmonth ="Jul",postyear=2022
+        //  console.log("Post date = "+ postday+" "+ postmonth+" "+postyear +"  "+" 03:04:33")
+
+
+//curent date ///
+var currentDate= new Date()
+var crtYear=new Intl.DateTimeFormat('en', { year: 'numeric' }).format(currentDate);
+var crtDay=new Intl.DateTimeFormat('en', { day: '2-digit' }).format(currentDate);
+var crtMont= new Intl.DateTimeFormat('en', { month: 'short' }).format(currentDate);
+var crtHours=(currentDate.getHours())
+var crtMints=(currentDate.getMinutes())
+var crtSec=(currentDate.getSeconds())
+// console.log("Current Time = "+crtDay+" "+crtMont+" "+crtYear+"   "+ crtHours+":"+crtMints+":"+crtSec)
+
+
+
+
+
+
+
+console.log("show post date")
+// If date is today - 12h or 2h ago or 1m ago
+
+if (crtDay==postday){
+    if(crtHours==postHour&&crtMints==postmint){
+         return (crtSec-postSec +"s ago")
+    }else if(crtHours==postHour){
+         return (crtMints-postmint+"m ago")
+    }else {
+return (crtHours-postHour+"h ago")
+    }
+}
+////If date is current year - Jul 15 or Jan 1//
+
+else if(crtYear==postyear){
+    return (postmonth+' '+postday)
+//Else  - July 15 2020 or July 15 2019//
+}else{
+    return (postmonth+' '+postday+' '+postyear)
+}
+
+   
+}
+
+export default dateFormatter
