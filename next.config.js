9a484975e0ac5cbdc11b73200851efb983141aeb
ritikagarg <ritikagarg2101998@gmail.com>
2022-08-18 12:01:33 -0200

    create  env variable

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
   env: {
     BEARERTOKEN: "AAAAAAAAAAAAAAAAAAAAAMWGewEAAAAA2x6S4LF6Hgth69Bd5BbxxFk%2BBko%3DoIdt0OJapGlQv0Vn63sOtIypxejwRzjWXsGPbvmIJIYqEnBdfz"
   }
+  
 }
 
 module.exports = nextConfig
